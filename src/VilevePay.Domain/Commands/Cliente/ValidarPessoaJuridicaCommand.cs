7cb8986c227ae77c70d9b468685dc9239f8bce60
marcelocamargosjr <marcelocamargosjr@hotmail.com>
2020-09-14 14:11:09 -0300

    Add validar pessoa juridica

---
+++ src/VilevePay.Domain/Commands/Cliente/ValidarPessoaJuridicaCommand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using VilevePay.Domain.Validations.Cliente;
+
+namespace VilevePay.Domain.Commands.Cliente
+{
+    public class ValidarPessoaJuridicaCommand : ClienteCommand, IRequest<bool>
+    {
+        public ValidarPessoaJuridicaCommand(string codigoConvite, string cnpj)
+        {
+            CodigoConvite = codigoConvite;
+            Cnpj = cnpj;
+        }
+
+        public override bool IsValid()
+        {
+            ValidationResult = new ValidarPessoaJuridicaCommandValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+    }
+}
---
+++ src/VilevePay.Domain/Validations/Cliente/ValidarPessoaJuridicaCommandValidation.cs
@@ -0,0 +1,13 @@
+﻿using VilevePay.Domain.Commands.Cliente;
+
+namespace VilevePay.Domain.Validations.Cliente
+{
+    public class ValidarPessoaJuridicaCommandValidation : ClienteValidation<ValidarPessoaJuridicaCommand>
+    {
+        public ValidarPessoaJuridicaCommandValidation()
+        {
+            ValidateCodigoConvite();
+            ValidateCnpj();
+        }
+    }
+}
