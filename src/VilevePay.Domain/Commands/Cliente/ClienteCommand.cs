25909fa75bf9d554e7c8d2177b0a0e48cbde8280
marcelocamargosjr <marcelocamargosjr@hotmail.com>
2020-09-14 14:41:55 -0300

    validações clientes endereço

--- src/VilevePay.Domain/Commands/Cliente/ClienteCommand.cs
+++ src/VilevePay.Domain/Commands/Cliente/ClienteCommand.cs
@@ -8,5 +8,12 @@ namespace VilevePay.Domain.Commands.Cliente
         public string Cpf { get; protected set; }
         public string Cnpj { get; protected set; }
         public string ComprovanteBase64 { get; protected set; }
+        public string Cep { get; private set; }
+        public string Logradouro { get; private set; }
+        public int Numero { get; private set; }
+        public string Complemento { get; private set; }
+        public string Bairro { get; private set; }
+        public string Cidade { get; private set; }
+        public string Estado { get; private set; }
     }
 }
--- src/VilevePay.Domain/Validations/Cliente/ClienteValidation.cs
+++ src/VilevePay.Domain/Validations/Cliente/ClienteValidation.cs
@@ -31,5 +31,54 @@ namespace VilevePay.Domain.Validations.Cliente
             RuleFor(c => c.ComprovanteBase64)
                 .NotEmpty().WithMessage("O campo comprovante base64 é obrigatório.");
         }
+
+        protected void ValidateCep()
+        {
+            RuleFor(c => c.Cep)
+                .NotEmpty().WithMessage("O campo CEP é obrigatório.")
+                .Length(2, 100).WithMessage("O campo CEP deve ter entre 2 e 100 caracteres.");
+        }
+
+        protected void ValidateLogradouro()
+        {
+            RuleFor(c => c.Logradouro)
+                .NotEmpty().WithMessage("O campo logradouro é obrigatório.")
+                .Length(2, 100).WithMessage("O campo logradouro deve ter entre 2 e 100 caracteres.");
+        }
+
+        protected void ValidateNumero()
+        {
+            RuleFor(c => c.Numero)
+                .NotNull().WithMessage("O campo número é obrigatório.")
+                .GreaterThan(0).WithMessage("O campo número deve ser maior que zero.");
+        }
+
+        protected void ValidateComplemento()
+        {
+            RuleFor(c => c.Complemento)
+                .Length(2, 100).WithMessage("O campo complemento deve ter entre 2 e 100 caracteres.")
+                .When(e => !string.IsNullOrEmpty(e.Complemento));
+        }
+
+        protected void ValidateBairro()
+        {
+            RuleFor(c => c.Bairro)
+                .NotEmpty().WithMessage("O campo bairro é obrigatório.")
+                .Length(2, 100).WithMessage("O campo bairro deve ter entre 2 e 100 caracteres.");
+        }
+
+        protected void ValidateCidade()
+        {
+            RuleFor(c => c.Cidade)
+                .NotEmpty().WithMessage("O campo cidade é obrigatório.")
+                .Length(2, 100).WithMessage("O campo cidade deve ter entre 2 e 100 caracteres.");
+        }
+
+        protected void ValidateEstado()
+        {
+            RuleFor(c => c.Estado)
+                .NotEmpty().WithMessage("O campo estado é obrigatório.")
+                .Length(2, 100).WithMessage("O campo estado deve ter entre 2 e 100 caracteres.");
+        }
     }
 }
