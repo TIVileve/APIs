07d1e0794bf1ce205293e59136e5c8426096ec1a
marcelocamargosjr <marcelocamargosjr@hotmail.com>
2020-09-14 14:52:12 -0300

    Add registrar endereco

--- src/VilevePay.Domain/Commands/Cliente/ClienteCommand.cs
+++ src/VilevePay.Domain/Commands/Cliente/ClienteCommand.cs
@@ -8,12 +8,12 @@ namespace VilevePay.Domain.Commands.Cliente
         public string Cpf { get; protected set; }
         public string Cnpj { get; protected set; }
         public string ComprovanteBase64 { get; protected set; }
-        public string Cep { get; private set; }
-        public string Logradouro { get; private set; }
-        public int Numero { get; private set; }
-        public string Complemento { get; private set; }
-        public string Bairro { get; private set; }
-        public string Cidade { get; private set; }
-        public string Estado { get; private set; }
+        public string Cep { get; protected set; }
+        public string Logradouro { get; protected set; }
+        public int Numero { get; protected set; }
+        public string Complemento { get; protected set; }
+        public string Bairro { get; protected set; }
+        public string Cidade { get; protected set; }
+        public string Estado { get; protected set; }
     }
 }
---
+++ src/VilevePay.Domain/Validations/Cliente/RegistrarEnderecoCommandValidation.cs
@@ -0,0 +1,19 @@
+﻿using VilevePay.Domain.Commands.Cliente;
+
+namespace VilevePay.Domain.Validations.Cliente
+{
+    public class RegistrarEnderecoCommandValidation : ClienteValidation<RegistrarEnderecoCommand>
+    {
+        public RegistrarEnderecoCommandValidation()
+        {
+            ValidateCodigoConvite();
+            ValidateCep();
+            ValidateLogradouro();
+            ValidateNumero();
+            ValidateComplemento();
+            ValidateBairro();
+            ValidateCidade();
+            ValidateEstado();
+        }
+    }
+}
