a4633c1a920ad2fed9e66a3b4ad6068020f51a00
marcelocamargosjr <marcelocamargosjr@hotmail.com>
2020-09-14 14:27:16 -0300

    Add registrar comprovante pessoa juridica

---
+++ src/VilevePay.Domain/Commands/Cliente/RegistrarComprovantePessoaJuridicaCommand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using VilevePay.Domain.Validations.Cliente;
+
+namespace VilevePay.Domain.Commands.Cliente
+{
+    public class RegistrarComprovantePessoaJuridicaCommand : ClienteCommand, IRequest<bool>
+    {
+        public RegistrarComprovantePessoaJuridicaCommand(string codigoConvite, string comprovanteBase64)
+        {
+            CodigoConvite = codigoConvite;
+            ComprovanteBase64 = comprovanteBase64;
+        }
+
+        public override bool IsValid()
+        {
+            ValidationResult = new RegistrarComprovantePessoaJuridicaCommandValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+    }
+}
---
+++ src/VilevePay.Domain/Validations/Cliente/RegistrarComprovantePessoaJuridicaCommandValidation.cs
@@ -0,0 +1,13 @@
+﻿using VilevePay.Domain.Commands.Cliente;
+
+namespace VilevePay.Domain.Validations.Cliente
+{
+    public class RegistrarComprovantePessoaJuridicaCommandValidation : ClienteValidation<RegistrarComprovantePessoaJuridicaCommand>
+    {
+        public RegistrarComprovantePessoaJuridicaCommandValidation()
+        {
+            ValidateCodigoConvite();
+            ValidateComprovanteBase64();
+        }
+    }
+}
