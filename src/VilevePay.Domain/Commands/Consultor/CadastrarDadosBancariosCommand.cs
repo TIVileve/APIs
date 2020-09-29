b876bfc696055e9079c0cb9cc9e9d7d843a10291
marcelocamargosjr <marcelocamargosjr@hotmail.com>
2020-09-29 14:41:36 -0300

    Add CadastrarDadosBancariosCommand

---
+++ src/VilevePay.Domain/Commands/Consultor/CadastrarDadosBancariosCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using VilevePay.Domain.Validations.Consultor;
+
+namespace VilevePay.Domain.Commands.Consultor
+{
+    public class CadastrarDadosBancariosCommand : ConsultorCommand, IRequest<bool>
+    {
+        public CadastrarDadosBancariosCommand(string codigoConvite)
+        {
+            CodigoConvite = codigoConvite;
+        }
+
+        public override bool IsValid()
+        {
+            ValidationResult = new CadastrarDadosBancariosCommandValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+    }
+}
---
+++ src/VilevePay.Domain/Validations/Consultor/CadastrarDadosBancariosCommandValidation.cs
@@ -0,0 +1,12 @@
+﻿using VilevePay.Domain.Commands.Consultor;
+
+namespace VilevePay.Domain.Validations.Consultor
+{
+    public class CadastrarDadosBancariosCommandValidation : ConsultorValidation<CadastrarDadosBancariosCommand>
+    {
+        public CadastrarDadosBancariosCommandValidation()
+        {
+            ValidateCodigoConvite();
+        }
+    }
+}
