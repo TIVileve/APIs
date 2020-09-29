fcacefc3dcd353fdcf2afe112a3e35c687be18cc
marcelocamargosjr <marcelocamargosjr@hotmail.com>
2020-09-29 14:43:54 -0300

    Add CadastrarEmailCommand

---
+++ src/VilevePay.Domain/Commands/Consultor/CadastrarEmailCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using VilevePay.Domain.Validations.Consultor;
+
+namespace VilevePay.Domain.Commands.Consultor
+{
+    public class CadastrarEmailCommand : ConsultorCommand, IRequest<bool>
+    {
+        public CadastrarEmailCommand(string codigoConvite)
+        {
+            CodigoConvite = codigoConvite;
+        }
+
+        public override bool IsValid()
+        {
+            ValidationResult = new CadastrarEmailCommandValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+    }
+}
---
+++ src/VilevePay.Domain/Validations/Consultor/CadastrarEmailCommandValidation.cs
@@ -0,0 +1,12 @@
+﻿using VilevePay.Domain.Commands.Consultor;
+
+namespace VilevePay.Domain.Validations.Consultor
+{
+    public class CadastrarEmailCommandValidation : ConsultorValidation<CadastrarEmailCommand>
+    {
+        public CadastrarEmailCommandValidation()
+        {
+            ValidateCodigoConvite();
+        }
+    }
+}
