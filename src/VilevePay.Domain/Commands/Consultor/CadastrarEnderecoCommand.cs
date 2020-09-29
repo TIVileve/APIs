c011f1c0b1040473b2042b0c1bd592543e9d922b
marcelocamargosjr <marcelocamargosjr@hotmail.com>
2020-09-29 14:37:51 -0300

    Add CadastrarEnderecoCommand

---
+++ src/VilevePay.Domain/Commands/Consultor/CadastrarEnderecoCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using VilevePay.Domain.Validations.Consultor;
+
+namespace VilevePay.Domain.Commands.Consultor
+{
+    public class CadastrarEnderecoCommand : ConsultorCommand, IRequest<bool>
+    {
+        public CadastrarEnderecoCommand(string codigoConvite)
+        {
+            CodigoConvite = codigoConvite;
+        }
+
+        public override bool IsValid()
+        {
+            ValidationResult = new CadastrarEnderecoCommandValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+    }
+}
---
+++ src/VilevePay.Domain/Validations/Consultor/CadastrarEnderecoCommandValidation.cs
@@ -0,0 +1,12 @@
+﻿using VilevePay.Domain.Commands.Consultor;
+
+namespace VilevePay.Domain.Validations.Consultor
+{
+    public class CadastrarEnderecoCommandValidation : ConsultorValidation<CadastrarEnderecoCommand>
+    {
+        public CadastrarEnderecoCommandValidation()
+        {
+            ValidateCodigoConvite();
+        }
+    }
+}
