dfdd947555944ec0c6f0a7e6078f54ea8c6e65c0
marcelocamargosjr <marcelocamargosjr@hotmail.com>
2020-09-30 13:41:42 -0300

    correção segurança

--- src/VilevePay.Services.Api/Controllers/v1/AutorizacaoController.cs
+++ src/VilevePay.Services.Api/Controllers/v1/AutorizacaoController.cs
@@ -48,7 +48,7 @@ namespace VilevePay.Services.Api.Controllers.v1
         [HttpPost("convites/{codigoConvite}/senhas")]
         [ProducesResponseType((int)HttpStatusCode.NoContent)]
         [ProducesResponseType(typeof(IEnumerable<string>), (int)HttpStatusCode.BadRequest)]
-        public IActionResult CadastrarSenha(string codigoConvite, string email, string senha, string confirmarSenha)
+        public IActionResult CadastrarSenha(string codigoConvite, [FromHeader] string email, [FromHeader] string senha, [FromHeader] string confirmarSenha)
         {
             _autorizacaoAppService.CadastrarSenha(codigoConvite, email, senha, confirmarSenha);
 
