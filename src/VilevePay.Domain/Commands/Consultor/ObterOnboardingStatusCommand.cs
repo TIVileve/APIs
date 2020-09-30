f2b9b8c20e216f3e561fc943d104a99799db76e2
marcelocamargosjr <marcelocamargosjr@hotmail.com>
2020-09-29 21:16:25 -0300

    add obter status onboarding

---
+++ src/VilevePay.Domain/Commands/Consultor/ObterOnboardingStatusCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using VilevePay.Domain.Validations.Consultor;
+
+namespace VilevePay.Domain.Commands.Consultor
+{
+    public class ObterOnboardingStatusCommand : ConsultorCommand, IRequest<object>
+    {
+        public ObterOnboardingStatusCommand(string codigoConvite)
+        {
+            CodigoConvite = codigoConvite;
+        }
+
+        public override bool IsValid()
+        {
+            ValidationResult = new ObterOnboardingStatusCommandValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+    }
+}
---
+++ src/VilevePay.Domain/Validations/Consultor/ObterOnboardingStatusCommandValidation.cs
@@ -0,0 +1,12 @@
+﻿using VilevePay.Domain.Commands.Consultor;
+
+namespace VilevePay.Domain.Validations.Consultor
+{
+    public class ObterOnboardingStatusCommandValidation : ConsultorValidation<ObterOnboardingStatusCommand>
+    {
+        public ObterOnboardingStatusCommandValidation()
+        {
+            ValidateCodigoConvite();
+        }
+    }
+}
