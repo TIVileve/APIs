237c64c948cfb86df4492ce19e8d91ee19547906
marcelocamargosjr <marcelocamargosjr@hotmail.com>
2020-10-15 14:30:19 -0300

    CadastrarPessoaJuridicaCommand

---
+++ src/VilevePay.Domain/Commands/Consultor/CadastrarPessoaJuridicaCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using VilevePay.Domain.Validations.Consultor;
+
+namespace VilevePay.Domain.Commands.Consultor
+{
+    public class CadastrarPessoaJuridicaCommand : ConsultorCommand, IRequest<bool>
+    {
+        public CadastrarPessoaJuridicaCommand(string codigoConvite)
+        {
+            CodigoConvite = codigoConvite;
+        }
+
+        public override bool IsValid()
+        {
+            ValidationResult = new CadastrarPessoaJuridicaCommandValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+    }
+}
---
+++ src/VilevePay.Domain/Validations/Consultor/CadastrarPessoaJuridicaCommandValidation.cs
@@ -0,0 +1,12 @@
+﻿using VilevePay.Domain.Commands.Consultor;
+
+namespace VilevePay.Domain.Validations.Consultor
+{
+    public class CadastrarPessoaJuridicaCommandValidation : ConsultorValidation<CadastrarPessoaJuridicaCommand>
+    {
+        public CadastrarPessoaJuridicaCommandValidation()
+        {
+            ValidateCodigoConvite();
+        }
+    }
+}
