21ecf14d6aa40749f757a1616065f1137aabb278
marcelocamargosjr <marcelocamargosjr@hotmail.com>
2020-10-15 15:06:54 -0300

    api consultores

--- src/VilevePay.Services.Api/Controllers/v1/ConsultorController.cs
+++ src/VilevePay.Services.Api/Controllers/v1/ConsultorController.cs
@@ -30,6 +30,81 @@ namespace VilevePay.Services.Api.Controllers.v1
             _notifications = (DomainNotificationHandler)notifications;
         }
 
+        [HttpPost("convites/{codigoConvite}/dados-bancarios")]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType(typeof(IEnumerable<string>), (int)HttpStatusCode.BadRequest)]
+        public IActionResult CadastrarDadosBancarios(string codigoConvite, [FromBody] object dadosBancarios)
+        {
+            _consultorAppService.CadastrarDadosBancarios(codigoConvite);
+
+            if (IsValidOperation())
+            {
+                return NoContent();
+            }
+
+            return BadRequest(_notifications.GetNotifications().Select(n => n.Value));
+        }
+
+        [HttpPost("convites/{codigoConvite}/emails")]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType(typeof(IEnumerable<string>), (int)HttpStatusCode.BadRequest)]
+        public IActionResult CadastrarEmail(string codigoConvite, [FromBody] object email)
+        {
+            _consultorAppService.CadastrarEmail(codigoConvite);
+
+            if (IsValidOperation())
+            {
+                return NoContent();
+            }
+
+            return BadRequest(_notifications.GetNotifications().Select(n => n.Value));
+        }
+
+        [HttpPost("convites/{codigoConvite}/enderecos")]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType(typeof(IEnumerable<string>), (int)HttpStatusCode.BadRequest)]
+        public IActionResult CadastrarEndereco(string codigoConvite, [FromBody] object endereco)
+        {
+            _consultorAppService.CadastrarEndereco(codigoConvite);
+
+            if (IsValidOperation())
+            {
+                return NoContent();
+            }
+
+            return BadRequest(_notifications.GetNotifications().Select(n => n.Value));
+        }
+
+        [HttpPost("convites/{codigoConvite}/pessoas-juridicas")]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType(typeof(IEnumerable<string>), (int)HttpStatusCode.BadRequest)]
+        public IActionResult CadastrarPessoaJuridica(string codigoConvite, [FromBody] object pessoaJuridica)
+        {
+            _consultorAppService.CadastrarPessoaJuridica(codigoConvite);
+
+            if (IsValidOperation())
+            {
+                return NoContent();
+            }
+
+            return BadRequest(_notifications.GetNotifications().Select(n => n.Value));
+        }
+
+        [HttpPost("convites/{codigoConvite}/telefones")]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType(typeof(IEnumerable<string>), (int)HttpStatusCode.BadRequest)]
+        public IActionResult CadastrarTelefone(string codigoConvite, [FromBody] object telefone)
+        {
+            _consultorAppService.CadastrarTelefone(codigoConvite);
+
+            if (IsValidOperation())
+            {
+                return NoContent();
+            }
+
+            return BadRequest(_notifications.GetNotifications().Select(n => n.Value));
+        }
+
         [HttpGet("convites/{codigoConvite}/onboarding/status")]
         [ProducesResponseType(typeof(StatusOnboardingViewModel), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(IEnumerable<string>), (int)HttpStatusCode.BadRequest)]
