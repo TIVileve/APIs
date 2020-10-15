e67fff18831398245ff869e75e318b0341c95304
marcelocamargosjr <marcelocamargosjr@hotmail.com>
2020-10-15 14:37:43 -0300

    ConsultorCommandHandler

--- src/VilevePay.Domain/CommandHandlers/ConsultorCommandHandler.cs
+++ src/VilevePay.Domain/CommandHandlers/ConsultorCommandHandler.cs
@@ -9,6 +9,11 @@ using VilevePay.Domain.Interfaces;
 namespace VilevePay.Domain.CommandHandlers
 {
     public class ConsultorCommandHandler : CommandHandler,
+        IRequestHandler<CadastrarDadosBancariosCommand, bool>,
+        IRequestHandler<CadastrarEmailCommand, bool>,
+        IRequestHandler<CadastrarEnderecoCommand, bool>,
+        IRequestHandler<CadastrarPessoaJuridicaCommand, bool>,
+        IRequestHandler<CadastrarTelefoneCommand, bool>,
         IRequestHandler<ObterStatusOnboardingCommand, object>
     {
         public ConsultorCommandHandler(
@@ -17,6 +22,61 @@ namespace VilevePay.Domain.CommandHandlers
             INotificationHandler<DomainNotification> notifications)
             : base(uow, bus, notifications)
         {
+        }
+
+        public async Task<bool> Handle(CadastrarDadosBancariosCommand message, CancellationToken cancellationToken)
+        {
+            if (!message.IsValid())
+            {
+                NotifyValidationErrors(message);
+                return await Task.FromResult(false);
+            }
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> Handle(CadastrarEmailCommand message, CancellationToken cancellationToken)
+        {
+            if (!message.IsValid())
+            {
+                NotifyValidationErrors(message);
+                return await Task.FromResult(false);
+            }
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> Handle(CadastrarEnderecoCommand message, CancellationToken cancellationToken)
+        {
+            if (!message.IsValid())
+            {
+                NotifyValidationErrors(message);
+                return await Task.FromResult(false);
+            }
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> Handle(CadastrarPessoaJuridicaCommand message, CancellationToken cancellationToken)
+        {
+            if (!message.IsValid())
+            {
+                NotifyValidationErrors(message);
+                return await Task.FromResult(false);
+            }
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> Handle(CadastrarTelefoneCommand message, CancellationToken cancellationToken)
+        {
+            if (!message.IsValid())
+            {
+                NotifyValidationErrors(message);
+                return await Task.FromResult(false);
+            }
+
+            return await Task.FromResult(true);
         }
 
         public async Task<object> Handle(ObterStatusOnboardingCommand message, CancellationToken cancellationToken)
