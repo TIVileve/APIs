97519e953153cfa8d03acc0e8bbdedf1d4563012
marcelocamargosjr <marcelocamargosjr@hotmail.com>
2020-11-08 22:09:30 -0300

    Add HttpClientHelper

---
+++ src/VilevePay.Infra.CrossCutting.Io/Http/HttpClientHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace VilevePay.Infra.CrossCutting.Io.Http
+{
+    public class HttpClientHelper
+    {
+        public static async Task<T> OnGet<T>(HttpClient httpClient, string route)
+        {
+            var response = await httpClient.GetAsync(route);
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+                throw new HttpRequestException(responseContent);
+
+            return await response.Content.ReadAsAsync<T>();
+        }
+    }
+}
---
+++ src/VilevePay.Infra.CrossCutting.Io/Http/IHttpClient.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Http;
+
+namespace VilevePay.Infra.CrossCutting.Io.Http
+{
+    public interface IHttpAppService
+    {
+        HttpClient CreateClient(string baseUri);
+    }
+}
