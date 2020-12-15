91a616ced2ab2261fe757a7169f4fd8a1cf16595
marcelocamargosjr <marcelocamargosjr@hotmail.com>
2020-12-15 14:08:00 -0300

    CadastrarCartaoCreditoViewModel

--- src/VilevePay.Application/ViewModels/v1/Cliente/CadastrarCartaoCreditoViewModel.cs
+++ src/VilevePay.Application/ViewModels/v1/Cliente/CadastrarCartaoCreditoViewModel.cs
@@ -1,6 +1,13 @@
-﻿namespace VilevePay.Application.ViewModels.v1.Cliente
+﻿using System;
+
+namespace VilevePay.Application.ViewModels.v1.Cliente
 {
     public class CadastrarCartaoCreditoViewModel
     {
+        public string NomeTitularCartao { get; set; }
+        public string Cpf { get; set; }
+        public string NumeroCartao { get; set; }
+        public DateTime Validade { get; set; }
+        public string Cvc { get; set; }
     }
 }
