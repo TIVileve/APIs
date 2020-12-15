707a49e52459e46c91474835f7380cc152e7ac6a
marcelocamargosjr <marcelocamargosjr@hotmail.com>
2020-12-15 14:11:34 -0300

    CadastrarDebitoContaViewModel

--- src/VilevePay.Application/ViewModels/v1/Cliente/CadastrarDebitoContaViewModel.cs
+++ src/VilevePay.Application/ViewModels/v1/Cliente/CadastrarDebitoContaViewModel.cs
@@ -2,5 +2,9 @@
 {
     public class CadastrarDebitoContaViewModel
     {
+        public string BancoConvencionado { get; set; }
+        public string Agencia { get; set; }
+        public string Conta { get; set; }
+        public int DataCobranca { get; set; }
     }
 }
