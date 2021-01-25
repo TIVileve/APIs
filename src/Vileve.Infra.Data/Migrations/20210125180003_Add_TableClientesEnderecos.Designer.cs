807e0d9bfd4da60cf48b7c303cb6afe8472fc784
marcelocamargosjr <marcelocamargosjr@hotmail.com>
2021-01-25 15:00:20 -0300

    Add ClientesEnderecosTable

--- src/Vileve.Infra.Data/Migrations/20210125180003_Add_TableClientesEnderecos.Designer.cs
+++ src/Vileve.Infra.Data/Migrations/20210125180003_Add_TableClientesEnderecos.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Vileve.Infra.Data.Context;
 
 namespace Vileve.Infra.Data.Migrations
 {
     [DbContext(typeof(VileveContext))]
-    partial class VileveContextModelSnapshot : ModelSnapshot
+    [Migration("20210125180003_Add_TableClientesEnderecos")]
+    partial class Add_TableClientesEnderecos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Vileve.Infra.Data/Migrations/20210125180003_Add_TableClientesEnderecos.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vileve.Infra.Data.Migrations
+{
+    public partial class Add_TableClientesEnderecos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ClientesEnderecos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreationDate = table.Column<DateTime>(nullable: false),
+                    UpdateDate = table.Column<DateTime>(nullable: true),
+                    Cep = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true),
+                    Logradouro = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true),
+                    Numero = table.Column<int>(nullable: false),
+                    Complemento = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true),
+                    Bairro = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true),
+                    Cidade = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true),
+                    Estado = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true),
+                    ComprovanteBase64 = table.Column<string>(type: "varchar(MAX)", nullable: true),
+                    ClienteId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClientesEnderecos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ClientesEnderecos_Clientes_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Clientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClientesEnderecos_ClienteId",
+                table: "ClientesEnderecos",
+                column: "ClienteId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ClientesEnderecos");
+        }
+    }
+}
