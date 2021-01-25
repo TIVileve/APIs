0e66e69a7a4bef85c3b60d569f5eb30ae33e74a5
marcelocamargosjr <marcelocamargosjr@hotmail.com>
2021-01-25 13:24:22 -0300

    Add table ClientesProdutos

---
+++ src/Vileve.Infra.Data/Migrations/20210125162329_Add_TableClientesProdutos.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vileve.Infra.Data.Migrations
+{
+    public partial class Add_TableClientesProdutos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ClientesProdutos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreationDate = table.Column<DateTime>(nullable: false),
+                    UpdateDate = table.Column<DateTime>(nullable: true),
+                    CodigoProduto = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true),
+                    ClienteId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClientesProdutos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ClientesProdutos_Clientes_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Clientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClientesProdutos_ClienteId",
+                table: "ClientesProdutos",
+                column: "ClienteId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ClientesProdutos");
+        }
+    }
+}
