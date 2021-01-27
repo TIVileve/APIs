3b1d8603ff449c504be94f811d2b1cba005140a8
marcelocamargosjr <marcelocamargosjr@hotmail.com>
2021-01-27 17:48:23 -0300

    clean code

--- src/Vileve.Domain/Commands/Cliente/ClienteCommand.cs
+++ src/Vileve.Domain/Commands/Cliente/ClienteCommand.cs
@@ -20,14 +20,14 @@ namespace Vileve.Domain.Commands.Cliente
         public string CodigoProduto { get; protected set; }
 
         // Endereco
-        public string Cep { get; set; }
-        public string Logradouro { get; set; }
-        public int Numero { get; set; }
-        public string Complemento { get; set; }
-        public string Bairro { get; set; }
-        public string Cidade { get; set; }
-        public string Estado { get; set; }
-        public string ComprovanteBase64 { get; set; }
+        public string Cep { get; protected set; }
+        public string Logradouro { get; protected set; }
+        public int Numero { get; protected set; }
+        public string Complemento { get; protected set; }
+        public string Bairro { get; protected set; }
+        public string Cidade { get; protected set; }
+        public string Estado { get; protected set; }
+        public string ComprovanteBase64 { get; protected set; }
 
         // Dependente
         public Guid DependenteId { get; protected set; }
